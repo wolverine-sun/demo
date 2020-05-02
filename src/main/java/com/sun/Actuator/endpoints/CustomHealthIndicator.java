3fa41493093a2905fd8b649f95d41dd4dc13d130
wolverine-sun <sunyuhui_mail@163.com>
2020-05-02 10:51:34 +0800

    测试demo

---
+++ src/main/java/com/sun/Actuator/endpoints/CustomHealthIndicator.java
@@ -0,0 +1,13 @@
+package com.sun.Actuator.endpoints;
+
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomHealthIndicator implements HealthIndicator {
+    @Override
+    public Health health() {
+        return Health.up().withDetail("error","404").build();
+    }
+}
